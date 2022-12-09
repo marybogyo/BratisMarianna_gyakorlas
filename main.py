0b69bafb3da6dfe8ac8aa4d2abcef3463bf616c0
marybogyo <bratis.marianna@gmail.com>
2022-12-09 22:51:45 +0100

    potlas

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ vegyes.kiiras(";")
 
 vegyes.vel()
 vegyes.utolso()
+szam = int(input("Adj meg egy prím számot: "))
+vegyes.prim(szam)
+vegyes.kiiras()
+vegyes.ketjegy()
+vegyes.hany_primszam()
--- vegyes.py
+++ vegyes.py
@@ -2,7 +2,7 @@ import random
 #Készíts egy sorozat nevű tömböt
 # a következő elemekkel: -3, 5, 11, -2, 4
 sorozat = [-3, 5, 11, -2, 4]
-def kiiras(kiir):
+def kiiras(kiir = ""):
     i = 0
 
     szamok = ""
@@ -28,7 +28,30 @@ def utolso():
 
 
 #Egy paraméterében megadott számról eldönti, hogy prím-e?
-def prim():
-    szam = int(input("Adj meg egy prím számot: "))
+def prim(szam):
+    i = 2
+    while(i< szam and szam % i != 0):
+        i+=1
+    return i==szam
+print(prim(5))
+print(prim(22))
+
+def ketjegy():
+    i = 0
+    v = sorozat[0]
+    while not (v>=10):
+        i += 1
+        v = sorozat[i]
+    print(f"Az első kétjegyű szám {i}, értke: {v}")
+
+def hany_primszam():
+    i = 0
+    db = 0
+
+    while i < len(sorozat):
+        if prim(sorozat[i]):
+            db +=1
+        i += 1
+    print(f"Prím szám összesen: {db}")
 
 
